3a14976cfb232c710cbac59b8a8c496ee4fa6fcd
Camila Zapata <mzapataar@unal.edu.co>
2026-02-06 16:45:28 -0500

    feat: CRUD doctores, urls organizadas

---
+++ doctors/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import list_doctors, detail_doctor
+
+urlpatterns = [
+    path('doctors', list_doctors),
+    path('doctors/<int:pk>', detail_doctor)
+]
--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from .serializers import DoctorSerializer
+from .models import Doctor
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# GET /api/doctors => listar
+# POST /api/doctors => crear  
+# GET /api/doctors/<pk> => Detalle
+# PUT /api/doctors/<pk> => Actualizar
+# DELETE /api/doctors/<pk> => Eliminar
+
+@api_view(['GET', 'POST'])
+def list_doctors(request):
+    if request.method == 'GET':
+        doctors = Doctor.objects.all()
+        serializer = DoctorSerializer(doctors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'POST':
+        serializer = DoctorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET','PUT','DELETE'])
+def detail_doctor(request, pk):
+    try:
+        doctor = Doctor.objects.get(id=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = DoctorSerializer(doctor)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    if request.method == 'PUT':
+        serializer = DoctorSerializer(doctor, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    if request.method == 'DELETE':
+        doctor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
