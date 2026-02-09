bcdaec69b285127f1424d0b12f648b85b881eb7a
Camila Zapata <mzapataar@unal.edu.co>
2026-02-09 11:47:17 -0500

    feat: implementación de autenticación y permisos

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,10 +1,30 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .permissions import IsDoctor
+
 from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabilitySerializer, MedicalNoteSerializer
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    permission_classes = [IsAuthenticated, IsDoctor]
+    
+    @action(['post'], detail=True, url_path='set-on-vacation')
+    def set_on_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({'status': 'El doctor está de vacaciones'})
+    
+    @action(['post'], detail=True, url_path='set-off-vacation')
+    def set_off_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({'status': 'El doctor ya no está de vacaciones'})
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
--- patients/viewsets.py
+++ patients/viewsets.py
@@ -1,10 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .serializers import InsuranceSerializer, MedicalRecordSerializer, PatientSerializer
 from .models import Patient, Insurance, MedicalRecord
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+    @action(['GET'], detail=True, url_path='medical-records')
+    def get_medical_records(self, request, pk):
+        patient = self.get_object()
+        medical_records = patient.medical_history.all()
+        serializer = MedicalRecordSerializer(medical_records, many=True)
+        return Response(serializer.data)
 
 class InsuranceViewSet(viewsets.ModelViewSet):
     queryset = Insurance.objects.all()
