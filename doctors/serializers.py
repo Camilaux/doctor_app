9fad49c6df6bd0810d785c8c871a7da34022862f
Camila Zapata <mzapataar@unal.edu.co>
2026-02-09 12:25:23 -0500

    feat: validaciones custom

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -5,6 +5,18 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = '__all__' # esto es para incluir todos los campos del modelo Doctor, puedes especificar campos específicos si lo deseas
+    
+    def validate_email(self, value):
+        if "@example.com" in value:
+            return value
+        raise serializers.ValidationError("El correo electrónico debe ser del dominio @example.com")
+    
+    def validate(self, attrs):
+        if len(attrs['contact_number']) < 10 and attrs['is_on_vacation'] == True:
+            raise serializers.ValidationError(
+                "Por favor, ingrese un número de contacto válido antes de irte a vacaciones"
+            )
+        return super().validate(attrs)
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
--- patients/serializers.py
+++ patients/serializers.py
@@ -1,11 +1,28 @@
+from datetime import date
 from rest_framework import serializers
-
 from .models import Patient, Insurance, MedicalRecord
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = '__all__'  # esto es para incluir todos los campos del modelo Patient, puedes especificar campos específicos si lo deseas
+    
+    def validate_email(self, value):
+        if "@example.com" in value:
+            return value
+        raise serializers.ValidationError("El correo electrónico debe ser del dominio @example.com")
+    
+    def validate(self, attrs):
+        if len(attrs['contact_number']) < 10:
+            raise serializers.ValidationError(
+                "Por favor, ingrese un número de contacto válido"
+            )
+        return super().validate(attrs)
+    
+    def validate_birth_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("La fecha de nacimiento no puede ser en el futuro")
+        return value
 
 class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
